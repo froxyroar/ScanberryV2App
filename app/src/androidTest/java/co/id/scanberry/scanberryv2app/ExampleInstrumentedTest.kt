e8610028cc869809857dc363162a0c351e33e834
Pavelmez <pavel.manaf@gmail.com>
2025-05-21 03:48:50 +0700

    fix RealtimeScreen.kt and code cleanup

--- app/src/androidTest/java/co/id/scanberry/scanberryv2app/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/co/id/scanberry/scanberryv2app/ExampleInstrumentedTest.kt
@@ -2,8 +2,7 @@ package co.id.scanberry.scanberryv2app
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.*
-import org.junit.jupiter.api.Test
+import org.junit.Assert.assertEquals
 import org.junit.runner.RunWith
 
 /**
@@ -13,7 +12,6 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
